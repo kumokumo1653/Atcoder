9239fa109694b113470cdf2b20a17631cfd933d7
kumokumo1653 <fjsda526hdbs@gmail.com>
2021-06-15 10:23:14 +0900

    boot

---
+++ BootCamp/C-Lower.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n - 1; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define FORR(i,m,n) for(int i = m - 1; i >= n; i--)
+#define ALL(v) v.begin(), v.end()
+#define itn int
+#define Yes() cout << "Yes" << endl
+#define No() cout << "No" << endl
+#define YES() cout << "YES" << endl
+#define NO() cout << "NO" << endl
+#define println(x) cout << x << endl
+#define print(x) cout << x << " "
+template<typename T, typename U>
+inline bool CMAX(T &m, U x) { if (m < x) { m = x; return true; } return false; }
+template<typename T, typename U>
+inline bool CMIN(T &m, U x) { if (m > x) { m = x; return true; } return false; }
+
+typedef long long lint;
+typedef long double ldouble;
+const int INF = 1e9;
+const lint LINF = 1e18;
+const int MOD = 1e9+7;
+int main(){
+    itn n;
+    cin >> n;
+    vector<int> h(n);
+    int max = 0;
+    int cnt = 0;
+    int index = 0;
+    REP(i,n){
+        cin >> h[i];
+    }
+
+
+    FOR(i, 1, n){
+        if(h[index] >= h[i]){
+            cnt++;
+        }else{
+            CMAX(max, cnt);
+            cnt = 0;
+        }
+        
+        index = i;
+    }
+    CMAX(max, cnt);
+
+    println(max);
+
+    return 0;
+}
